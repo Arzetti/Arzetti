6f20f90939aee25fdfa6e52dccb40f608c813454
Arzetti <144374848+Arzetti@users.noreply.github.com>
2023-09-30 23:01:52 +0800

    Add files via upload

---
+++ Ray.js
@@ -0,0 +1,31 @@
+function showSubmitAlert() {
+    alert("Formulir telah berhasil disubmit!");
+}
+
+function confirmDelete() {
+    if (confirm("Apakah Anda yakin ingin menghapus data ini?")) {
+      alert("Data telah dihapus."); 
+    } else {
+
+    }
+  }
+
+  const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+  const appendAlert = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+      `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+      `   <div>${message}</div>`,
+      '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+      '</div>'
+    ].join('')
+  
+    alertPlaceholder.append(wrapper)
+  }
+  
+  const alertTrigger = document.getElementById('liveAlertBtn')
+  if (alertTrigger) {
+    alertTrigger.addEventListener('click', () => {
+      appendAlert('Nice, you triggered this alert message!', 'success')
+    })
+  }
